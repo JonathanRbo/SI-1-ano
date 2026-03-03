284b751402b090a68a37c697a8e9af020990c358
Jonathan Ribeiro <147669492+JonathanRbo@users.noreply.github.com>
2026-03-03 19:43:55 -0300

    Add files via upload

---
+++ APPC/Aula 02-03-2026/Testes.py
@@ -0,0 +1,53 @@
+# VERSION 1
+'''
+print ('Programa para calcular areas de quadrados\n')
+
+Lado_Texto=input('Digite quanto mede em cm o lado do quadrado desejado: ')
+
+Lado_Numero = float(Lado_Texto)
+
+area = Lado_Numero * Lado_Numero
+
+print('\nA area do quadrado indicado mede', area, 'centimetro quadrado')
+
+print('\nPrograma encerrado com sucesso')
+'''
+
+
+# VERSION 2
+'''
+print ('Programa para calcular areas de quadrados\n')
+
+Lado_Numero=float(input("Digite quanto mede em cm o lado do quadrado desejado:"))
+
+area = Lado_Numero * Lado_Numero
+
+print('\nA area do quadrado indicado mede', area, 'centimetro quadrado')
+
+print('\nPrograma encerrado com sucesso')
+'''
+
+
+# VERSION 3
+print ('Programa para calcular areas de quadrados\n')
+
+print("Digite quanto mede em cm o lado do quadrado desejado:",end="")
+Lado_Numero=float(input())
+
+area = Lado_Numero * Lado_Numero
+
+print('\nA area do quadrado indicado mede', area, 'centimetro quadrado',sep="")
+
+print('\nPrograma encerrado com sucesso')
+
+
+
+# Atividade para aprender mais: Como se converte graus celsius em graus firenrite
+
+
+
+
+
+
+
+
