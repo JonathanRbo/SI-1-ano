8b6867273da9e518d13ddb2f290131d9664d1b1d
Jonathan Ribeiro <147669492+JonathanRbo@users.noreply.github.com>
2026-03-10 20:36:33 -0300

    Revise SQL script for database Praticar1

--- ExpBD/aula-2026-03-10/Praticar1.sql
+++ ExpBD/aula-2026-03-10/Praticar1.sql
@@ -1,4 +1,4 @@
-create database Praticar1;
+use BD240226113;
 
 create table AUTORES (
     id_autor int,
@@ -6,7 +6,6 @@ create table AUTORES (
     pais varchar(50),
     ano_nascimento int
 );
-
 create table LIVROS (
 	id_livro int,
     titulo varchar(150),
@@ -14,14 +13,12 @@ create table LIVROS (
     ano_publicacao int,
     id_autor int
 );
-
 create table LEITORES  (
 	id_leitor int,
     nome varchar(100),
 	email varchar(100),
 	telefone varchar(20)
 );
-
 create table EMPRESTIMOS (
 	id_emprestimo int,
 	id_leitor int,
@@ -31,20 +28,13 @@ create table EMPRESTIMOS (
 );
 
 ALTER TABLE LIVROS ADD quantidade_estoque int;
-
 ALTER TABLE LEITORES ADD data_cadastro date;
-
 ALTER TABLE LEITORES MODIFY telefone VARCHAR(30);
-
 ALTER TABLE AUTORES DROP COLUMN pais;
-
 ALTER TABLE LIVROS ADD editora VARCHAR(100);
 
 DROP TABLE EMPRESTIMOS;
-
 DROP TABLE LIVROS;
-
 DROP TABLE AUTORES;
-
 
 
