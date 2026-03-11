4ad9956644fc3654c8a94382bc76cc790e3aecfb
Jonathan Ribeiro <147669492+JonathanRbo@users.noreply.github.com>
2026-03-11 13:23:28 -0300

    Update praticando.py

--- APPC/Aula-2026-03-09/praticando.py
+++ APPC/Aula-2026-03-09/praticando.py
@@ -1,13 +1,11 @@
 nota=9.5
 frequencia=0.85
-Print(nota<=5 and frequencia<0.75) #aprovado
+print(nota>=5 and frequencia>=0.75) #aprovado
 
 nota=4.5
 frequencia=0.55
-Print(nota>=5 and frequencia<0.75) #Reprovado
+print(nota<5 or frequencia<0.75) #Reprovado
 
 nota=4.5
 frequencia=0.25
-Print(nota>=5 and frequencia=0.75) #Reprovado
-
-
+print(nota<5 or frequencia<0.75) #Reprovado
